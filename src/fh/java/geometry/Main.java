6f780f0928026b68c4c1ecd93e430eea758914fb
hribernn15 <nikolaus.hribernig@edu.fh-joanneum.at>
2016-11-04 16:28:04 +0100

    Prints

--- src/fh/java/geometry/Main.java
+++ src/fh/java/geometry/Main.java
@@ -10,17 +10,58 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-		//TODO: main() is missing
+
+
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        main.doIt();
+
+        //fillList();
+    }
+
+    private void doIt() {
+
+        List<GeometricShape> shapeList = new ArrayList<>();
+
+        fillList(shapeList);
+
+        printList(shapeList);
+
+    }
 
 
     public void fillList(List<GeometricShape> shapeList) {
-		//TODO: Implement
+
+        shapeList.add(new Circle(1,2,2.0));
+        shapeList.add(new Circle(2,1,2.0));
+        shapeList.add(new Circle(5,3,7.0));
+
+        shapeList.add(new RightTriangle(11,22,10.0,11.0));
+
+        shapeList.add(new Rectangle(1,2,20.1,21.11));
 
     }
 
 
     public void printList(List<GeometricShape> shapeList) {
-		//TODO: Implement
+
+        System.out.println("--- for loop ---");
+        for (int pos = 0; pos < shapeList.size(); pos++) {
+            System.out.println(shapeList.get(pos));
+        }
+
+        System.out.println("--- for each ---");
+
+        for (GeometricShape shape : shapeList) {
+            System.out.println(shape);
+        }
+        System.out.println("--- for each ---");
+        shapeList.stream().forEach(shape-> System.out.println(shape));
+        System.out.println("--- for each ---");
+
+        shapeList.stream().forEach(System.out::println);
 
     }
 
